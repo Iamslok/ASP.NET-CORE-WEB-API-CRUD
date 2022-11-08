dec95b0e473e87bfec7425937fc6b1fd8d1c9f9f
vineeth kumar <vineethkumarss19@gmail.com>
2022-11-08 17:45:33 +0530

    set-up

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_API.Models;
+
+namespace WEB_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<UserDetails> Users { get; set; }
+    }
+}
